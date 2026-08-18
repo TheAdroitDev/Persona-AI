6bf7b116ad8e340f8c1bd8477ed9828a8d7ca20c
GitPirates <starshivamverma2@gmail.com>
2026-08-18 22:19:56 +0530

    feat: add Accordion component imports to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
 
+import "./globals.css"
 export default function Home() {
+
     return (
         <>
             <div>
-
+                 
             </div>
         </>
     );
